5227cbf49d10001b1a86f9253e9c76228c3312cf
Renan Carvalho <renan@interprocess.com.br>
2021-10-14 06:36:33 -0300

    feat:  Adiciona rota para room

--- src/route.js
+++ src/route.js
@@ -1,11 +1,17 @@
 const questionController = require('./controllers/QuestionController');
+const roomController = require('./controllers/RoomController');
 const express = require('express');
 
 const route = express.Router();
 
-route.get('/', (req,res) => {res.render('index')});
-route.get('/room', (req,res)=>{res.render('room')});
-route.get('/create-pass', (req,res)=>{res.render('create-pass')});
+route.get('/', (req,res) => {res.render('index', {page:'enter-room'})});
+
+route.get('/create-pass', (req,res)=>{res.render('index', {page:'create-pass'})});
+
+route.get('/room/:room', (req,res)=>{res.render('room')});
+
 route.post('/room/:room/:question/:action',questionController.index);
+
+route.post('/room/create-room', roomController.create);
 
 module.exports = route;
