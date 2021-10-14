486adf03cea7d23330a632e19a97f45570cb3b1b
Renan Carvalho <renan@interprocess.com.br>
2021-10-14 06:18:18 -0300

    feat: Adiciona efeito de abri  o modal de acordo com a action  executada

--- src/route.js
+++ src/route.js
@@ -1,8 +1,11 @@
+const questionController = require('./controllers/QuestionController');
 const express = require('express');
+
 const route = express.Router();
 
 route.get('/', (req,res) => {res.render('index')});
 route.get('/room', (req,res)=>{res.render('room')});
 route.get('/create-pass', (req,res)=>{res.render('create-pass')});
+route.post('/room/:room/:question/:action',questionController.index);
 
 module.exports = route;
