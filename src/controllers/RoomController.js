661d1083bf0dc35f11b470011994e04611d359a6
renan <renancarvalhodesouza@gmail.com>
2021-10-17 20:57:43 -0300

    feat: Salvar registros no banco de dados

--- src/controllers/RoomController.js
+++ src/controllers/RoomController.js
@@ -1,12 +1,37 @@
-module.exports = {
-    create(req,res) {
-        
-        let roomId = 0;
+const Database = require('../db/config');
 
-        for(var i = 0; i<6; i++){
-            roomId += Math.floor(Math.random() * 10).toString();
+function getRandom() {
+    return Math.floor(Math.random() * 10).toString();
+}
+
+module.exports = {
+
+    async create(req, res) {
+
+        const db = await Database();
+
+        let roomId = getRandom();
+        const pass = req.body.password;
+
+        for (var i = 0; i < 6; i++) {
+            roomId += getRandom();
         }
+        debugger;
+
+        await db.run(`INSERT INTO rooms 
+        (
+            id,
+            pass
+        ) VALUES (
+            "${parseInt(roomId)}",
+            "${pass}"
+             );`);
+
+        await db.close();
 
         res.redirect(`/room/${roomId}`);
     }
 }
+
+
+
