79810660f4264fb6d313359df2dfa3f9450193fd
renan <renancarvalhodesouza@gmail.com>
2021-10-17 20:27:36 -0300

    chore: Adiciona modo de redirecionar o usuário para tela inicial

--- src/controllers/RoomController.js
+++ src/controllers/RoomController.js
@@ -1,6 +1,12 @@
 module.exports = {
     create(req,res) {
-        const roomId = req.body.password;
+        
+        let roomId = 0;
+
+        for(var i = 0; i<6; i++){
+            roomId += Math.floor(Math.random() * 10).toString();
+        }
+
         res.redirect(`/room/${roomId}`);
     }
 }
